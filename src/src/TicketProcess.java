b2738d925b4f54c3ce80823a001aba094cb48f90
okimjj <jjrocks303@naver.com>
2022-04-21 20:08:19 +0900

    with comments

--- src/src/TicketProcess.java
+++ src/src/TicketProcess.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class TicketProcess {
-	OrderVariables variables;
-	ArrayList <OrderVariables> orderList = new ArrayList <OrderVariables>(); 
-	OrderStage ordStage = new OrderStage();
-	AgeCalculator ageCal = new AgeCalculator();
-	TicketChoice tickChoice = new TicketChoice();
-	Discount dc = new Discount();
-	RepeatCheck rpCheck = new RepeatCheck();
-	EndPrint end = new EndPrint();
+	OrderVariables variables;//입력 데이터 클래스
+	ArrayList <OrderVariables> orderList = new ArrayList <OrderVariables>(); //구매 목록 ArrayList 
+	OrderStage ordStage = new OrderStage(); //주문 정보 입력 클래스
+	AgeCalculator ageCal = new AgeCalculator(); //나이 계산 클래스
+	TicketChoice tickChoice = new TicketChoice(); //권종 선택에 따른 가격 계산 클래스
+	Discount dc = new Discount(); //할인 조건에 따른 가격 계산 클래스
+	RepeatCheck rpCheck = new RepeatCheck(); // 반복 여부 확인 클래스
+	EndPrint end = new EndPrint(); //최종 구매 결과 출력 클래스
 	
 	void run() {
 		
@@ -36,16 +36,16 @@ public class TicketProcess {
 		
 		end.ending();
 		
-		for (OrderVariables items : orderList) {
+		for (OrderVariables items : orderList) { //구매 최종 결과 출력
 			
-			System.out.printf("%5s\t", items.ticketTypeName);
-			System.out.printf("%3s\t", items.ageGroup);
-			System.out.printf("%d장\t", items.numberOfTicket);
-			System.out.printf("%7d원\t", items.price);
-			System.out.printf("%s\n", items.discountType);
+			System.out.printf("%5s\t", items.ticketTypeName); //고객이 선택한 권종
+			System.out.printf("%3s\t", items.ageGroup); //고객의 연령대
+			System.out.printf("%d장\t", items.numberOfTicket); //주문한 티켓 수량
+			System.out.printf("%7d원\t", items.price); //가격
+			System.out.printf("%s\n", items.discountType); //적용 받은 할인의 종류
 		}
 		
 		System.out.printf("==================================\n");
-		System.out.printf("총 결제액은 %d원입니다. 감사합니다.", variables.priceFinal);
+		System.out.printf("총 결제액은 %d원입니다. 감사합니다.", variables.priceFinal); //최종 총액 출력
 	}
 }
